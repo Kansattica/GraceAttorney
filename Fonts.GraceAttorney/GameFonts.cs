ab3e29ab9fd9af81ceaab5ea211e2c0d3d59d1af
Grace H <Grace@hypnovir.us>
2020-07-09 12:09:44 -0700

    This seems like a more reasonable size for the name tag.

--- Fonts.GraceAttorney/GameFonts.cs
+++ Fonts.GraceAttorney/GameFonts.cs
@@ -8,6 +8,6 @@ namespace Fonts.GraceAttorney
     {
 		//currently, thank you George Douros's Unicode Fonts for Ancient Scripts
 		public static DynamicSpriteFont Dialogue { get; private set; } = DynamicSpriteFont.FromTtf(File.ReadAllBytes(Path.Combine("Content", "Fonts", "Aroania.ttf")), 35);
-		public static DynamicSpriteFont NameTag { get; private set; } = DynamicSpriteFont.FromTtf(File.ReadAllBytes(Path.Combine("Content", "Fonts", "AroaniaBold.ttf")), 32);
+		public static DynamicSpriteFont NameTag { get; private set; } = DynamicSpriteFont.FromTtf(File.ReadAllBytes(Path.Combine("Content", "Fonts", "AroaniaBold.ttf")), 28);
     }
 }
