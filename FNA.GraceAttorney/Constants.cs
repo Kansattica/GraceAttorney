3e6996ccd309b0012cc6d47593f4ed64aaa52fb2
Grace H <Grace@hypnovir.us>
2020-07-09 12:25:36 -0700

    Add note about sprite layers.

--- FNA.GraceAttorney/Constants.cs
+++ FNA.GraceAttorney/Constants.cs
@@ -4,12 +4,12 @@ using System.Text;
 
 namespace FNA.GraceAttorney
 {
+	// note that these are in render order: background, then sprites, then dialogue, and so on.
 	enum SpriteLayers { Background, CharacterSprites, DialogueBox, VeryTop}
 	static class Constants
 	{
 		public const int CharactersPerLine = 40;
 		public const int ExpectedLineCount = 3;
 		public const int CharactersPerDialogueBox = CharactersPerLine * ExpectedLineCount;
-
 	}
 }
