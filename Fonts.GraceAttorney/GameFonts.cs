575af5007b73ad75054596b408e663f017e3a134
Grace H <Grace@hypnovir.us>
2020-07-09 11:52:06 -0700

    That should be a good default font size.

--- Fonts.GraceAttorney/GameFonts.cs
+++ Fonts.GraceAttorney/GameFonts.cs
@@ -7,7 +7,7 @@ namespace Fonts.GraceAttorney
     public class GameFonts
     {
 		//currently, thank you George Douros's Unicode Fonts for Ancient Scripts
-		public static DynamicSpriteFont Dialogue { get; private set; } = DynamicSpriteFont.FromTtf(File.ReadAllBytes(Path.Combine("Content", "Fonts", "Aroania.ttf")), 36);
+		public static DynamicSpriteFont Dialogue { get; private set; } = DynamicSpriteFont.FromTtf(File.ReadAllBytes(Path.Combine("Content", "Fonts", "Aroania.ttf")), 35);
 		public static DynamicSpriteFont NameTag { get; private set; } = DynamicSpriteFont.FromTtf(File.ReadAllBytes(Path.Combine("Content", "Fonts", "AroaniaBold.ttf")), 32);
     }
 }
