91fc1398f6de245239c4c05934b2559cd975be86
Grace H <Grace@hypnovir.us>
2020-07-15 15:00:53 -0700

    Add text justification to the DialogueComponent.

--- GraceAttorney/Components/DialogueComponent.cs
+++ GraceAttorney/Components/DialogueComponent.cs
@@ -6,12 +6,14 @@ using Encompass;
 namespace GraceAttorney.Components
 {
 	enum NameTagLocation { Left, Center, Right };
+	enum JustifyText { Left, Center };
 	struct DialogueComponent : IComponent, IDrawableComponent
 	{
 		public bool Display;
 		public string Speaker;
 		public string Dialogue;
 		public NameTagLocation NameTagLocation;
+		public JustifyText Justification;
 
 		public int Layer { get; set; }
 	}
