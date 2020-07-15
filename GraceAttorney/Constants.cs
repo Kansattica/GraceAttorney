d4f1ac4811264a33c516e458dda9662d61924a89
Grace H <Grace@hypnovir.us>
2020-07-15 12:35:24 -0700

    Add note about characters per line.

--- GraceAttorney/Constants.cs
+++ GraceAttorney/Constants.cs
@@ -10,6 +10,9 @@ namespace GraceAttorney
 	{
 		public const int CharactersPerLine = 40;
 		public const int ExpectedLineCount = 3;
+
+		// Note that this only applies to left-justified "normal" dialogue.
+		// centered text doesn't wrap, so use CharactersPerLine for it.
 		public const int CharactersPerDialogueBox = CharactersPerLine * ExpectedLineCount;
 	}
 }
