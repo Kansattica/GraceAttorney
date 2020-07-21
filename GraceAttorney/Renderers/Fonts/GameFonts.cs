95056f46e5b92706431b25fa47ce5621132685a6
Grace H <Grace@hypnovir.us>
2020-07-20 20:43:44 -0700

    Rename the fonts to what they are.

--- GraceAttorney/Renderers/Fonts/GameFonts.cs
+++ GraceAttorney/Renderers/Fonts/GameFonts.cs
@@ -7,7 +7,8 @@ namespace GraceAttorney.Renderers.Fonts
     public class GameFonts
     {
 		//currently, thank you George Douros's Unicode Fonts for Ancient Scripts
-		public static DynamicSpriteFont Dialogue { get; private set; } = DynamicSpriteFont.FromTtf(File.ReadAllBytes(Path.Combine("Content", "Fonts", "Aroania.ttf")), 35);
-		public static DynamicSpriteFont NameTag { get; private set; } = DynamicSpriteFont.FromTtf(File.ReadAllBytes(Path.Combine("Content", "Fonts", "AroaniaBold.ttf")), 28);
+		// (Aroania for Dialogue, AroaniaBold for name tag)
+		public static DynamicSpriteFont Dialogue { get; private set; } = DynamicSpriteFont.FromTtf(File.ReadAllBytes(Path.Combine("Content", "Fonts", "Dialogue.ttf")), 35);
+		public static DynamicSpriteFont NameTag { get; private set; } = DynamicSpriteFont.FromTtf(File.ReadAllBytes(Path.Combine("Content", "Fonts", "NameTag.ttf")), 28);
     }
 }
